188d3b1f8684fbb6d71904a1f252fabc0998e1af
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-03 11:09:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let age = document.querySelector("#age");
+let namee = document.querySelector("#name");
+let submit = document.querySelector("#btn");
+
+const Ashish = (age, namee) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(age >= 18) {
+            resolve(`Welcome, ${namee}. You can vote`);
+        } else {
+            reject(`Oh sorry ${namee}, You aren't old enough`);
+        }
+    }, 4000);
+});
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(namee.value != "" && age.value != ""){
+        Ashish(age.value, namee.value)
+        .then(data => alert(data))
+        .catch(err => alert(err));
+    }
+});
