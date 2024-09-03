94a245544b3f651f5f4dfb707168d1a1a03970a6
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-03 13:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
   let eligibilityPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if(age >= 18) {
-        resolve(`Welcome, ${name}. You can vote`);
+        resolve(`Welcome, ${name}. You can vote.`);
       } else {
-        reject(`Oh sorry ${name}, You aren't old enough`);
+        reject(`Oh sorry ${name}, You aren't old enough.`);
       }
     }, 4000);
   });
